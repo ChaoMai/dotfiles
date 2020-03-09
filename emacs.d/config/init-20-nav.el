926ca197d27e9ca36202dea93b4a0618163da10e
chaomai <loneymai@gmail.com>
2020-03-09 23:06:23 +0800

    remove emacs.d

--- emacs.d/config/init-20-nav.el
+++
@@ -1,8 +0,0 @@
-(require 'use-package)
-
-(use-package evil
-             :ensure t
-             :diminish undo-tree-mode
-             :init
-             (evil-mode 1)
-             (setq sentence-end-double-space nil))
