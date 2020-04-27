17814aace32614c8d8d74678a13446b782c58792
chaomai <loneymai@gmail.com>
2020-04-27 18:58:24 +0800

    update emacs config

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -125,14 +125,11 @@
           (mapcar (lambda (y)
                     (if (cdr y) y
                       (list (pyim-cregexp-build-1 (car y)))) x))
-        (pyim-cregexp-build x))))
+        (pyim-cregexp-build-1 x))))
 
-  (setq ivy-re-builders-alist '((counsel-rg . eh-ivy-cregexp)
-                                (counsel-recentf . eh-ivy-cregexp)
-                                (counsel-projectile-rg . ivy--regex-plus)
-                                (counsel-projectile-find-file . eh-ivy-cregexp)
+  (setq ivy-re-builders-alist '((counsel-projectile-rg . ivy--regex-plus)
                                 (swiper . ivy--regex-plus)
-                                (t . ivy--regex-fuzzy))))
+                                (t . eh-ivy-cregexp))))
 
 (use-package! counsel
   :defer t
@@ -423,39 +420,39 @@
   :config
   ;; 激活 basedict 拼音词库，五笔用户请继续阅读 README
   (use-package pyim-basedict
-    :ensure nil
     :config (pyim-basedict-enable))
 
-  ;; (setq default-input-method "pyim")
-
   ;; 我使用全拼
-  (setq pyim-default-scheme 'quanpin)
+  (setq pyim-default-scheme 'quanpin
+        default-input-method "pyim"
+
+        ;; 开启拼音搜索功能
+        ;; pyim-isearch-mode 1
+
+        ;; 使用 popup-el 来绘制选词框, 如果用 emacs25, 建议设置
+        ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
+        ;; 手动安装 posframe 包。
+        pyim-page-tooltip 'posframe
+
+        ;; 选词框显示5个候选词
+        pyim-page-length 5
+        pyim-fuzzy-pinyin-alist '(("an" "ang")
+                                  ("in" "ing")
+                                  ("en" "eng")
+                                  ("uan" "uang")))
 
   ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
   ;; 我自己使用的中英文动态切换规则是：
   ;; 1. 光标只有在注释里面时，才可以输入中文。
   ;; 2. 光标前是汉字字符时，才能输入中文。
-  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-  (setq-default pyim-english-input-switch-functions
-                '(pyim-probe-dynamic-english
-                  pyim-probe-isearch-mode
-                  pyim-probe-program-mode
-                  pyim-probe-org-structure-template))
+  ;; 3. 使用 C-; 快捷键，强制将光标前的拼音字符串转换为中文。
+  (setq-default pyim-english-input-switch-functions '(pyim-probe-dynamic-english
+                                                      pyim-probe-isearch-mode
+                                                      pyim-probe-program-mode
+                                                      pyim-probe-org-structure-template)
 
-  (setq-default pyim-punctuation-half-width-functions
-                '(pyim-probe-punctuation-line-beginning
-                  pyim-probe-punctuation-after-punctuation))
-
-  ;; 开启拼音搜索功能
-  ;; (pyim-isearch-mode 1)
-
-  ;; 使用 popup-el 来绘制选词框, 如果用 emacs26, 建议设置
-  ;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
-  ;; 手动安装 posframe 包。
-  (setq pyim-page-tooltip 'posframe)
-
-  ;; 选词框显示5个候选词
-  (setq pyim-page-length 5)
+                pyim-punctuation-half-width-functions '(pyim-probe-punctuation-line-beginning
+                                                        pyim-probe-punctuation-after-punctuation))
 
   :bind
   (("C-;" . pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
@@ -467,6 +464,7 @@
   (pyim-greatdict-enable))
 
 (use-package! pangu-spacing
+  :demand t
   :config
   (global-pangu-spacing-mode 1)
   (setq pangu-spacing-real-insert-separtor t))
@@ -486,7 +484,8 @@
   (awesome-tab-mode t)
   (setq awesome-tab-height 125))
 
-(use-package! posframe)
+(use-package! posframe
+  :demand t)
 
 (use-package! flycheck-posframe
   :after flycheck
