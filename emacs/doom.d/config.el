042e8fbcc7f3629d49289080757857d9fe98ed4c
chaomai <loneymai@gmail.com>
2020-08-28 10:12:21 +0800

    update

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -119,15 +119,13 @@
 
 (cond
  ((string-equal platform MACOS)
-  (setq doom-font (font-spec :family "IBM Plex Mono" :size 14 :weight 'regular)))
-  ;; (setq doom-variable-pitch-font (font-spec :family "Sarasa Mono SC" :size 14 :weight 'regular)))
+  (setq doom-font (font-spec :family "Cascadia Code" :size 15 :weight 'regular)))
 
  ((string-equal platform LINUX)
   (message "no implemented"))
 
  ((string-equal platform WSL)
-  (setq doom-font (font-spec :family "IBM Plex Mono" :size 14 :weight 'regular))))
-  ;; (setq doom-variable-pitch-font (font-spec :family "Sarasa Mono SC" :size 18 :weight 'regular))))
+  (setq doom-font (font-spec :family "Cascadia Code" :size 15 :weight 'regular))))
 
 (setq fancy-splash-image (concat doom-private-dir "doom.jpg"))
 
