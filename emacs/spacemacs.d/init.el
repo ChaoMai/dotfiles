29211052088ffd1953936c01bf62d0f3c6463658
chaomai <loneymai@gmail.com>
2020-10-11 22:52:40 +0800

    update spacemacs

--- emacs/spacemacs.d/init.el
+++ emacs/spacemacs.d/init.el
@@ -66,7 +66,6 @@ This function should only modify configuration layer settings."
      quickurl
      rust
      shell-scripts
-     spell-checking
      sql
      syntax-checking
      tabs
@@ -957,3 +956,22 @@ before packages are loaded."
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(pyim-dicts
+   '((:name "pyim-greatdict" :file "~/Documents/workspace/dotfiles/emacs/spacemacs.d/pyim-greatdict.pyim"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+)
