72dbc8af9a6bbe07bbc73d3806d55f91f7ca6e93
chaomai <loneymai@gmail.com>
2021-05-21 22:51:57 +0800

    update

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -155,7 +155,7 @@
 
 (cond
  ((string-equal platform MACOS)
-  (setq doom-font (font-spec :family "Fira Code" :size 13 :weight 'regular)))
+  (setq doom-font (font-spec :family "Menlo" :size 13 :weight 'regular)))
 
  ((string-equal platform LINUX)
   (message "no implemented"))
